a0070be48541fa023b71a4fe8d06bcad6448ad27
Florian Fida <f+git@fida.biz>
2018-10-11 18:21:59 +0200

    logging

--- src/Controllers/DefaultController.cs
+++ src/Controllers/DefaultController.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using YoutubeCollector.Db;
 using YoutubeCollector.Lib;
 
@@ -16,18 +18,28 @@ namespace YoutubeCollector.Controllers {
     public class DefaultController : ControllerBase {
         private readonly SettingsProvider _settingsProvider;
         private readonly Repository _repository;
+        private readonly ILogger<DefaultController> _logger;
+        private readonly IConfiguration _configuration;
 
         // GET api/values
         [HttpGet("/")]
         public ActionResult<string> Get() {
+
+            var c = string.Join(Environment.NewLine, _configuration.AsEnumerable().Select(kp => $"{kp.Key}={kp.Value}"));
+            c += Environment.NewLine + "PgConnectionString" + _settingsProvider.PgConnectionString;
+            c += Environment.NewLine + "PgHost" + _settingsProvider.PgHost;
+            _logger.LogCritical(c);
+
             var uh = new UrlHelper(ControllerContext);
             var link = uh.Action(nameof(GetVideoDetails), new {videoId="VIDEO_ID"});
             return $"try {link}";
         }
 
-        public DefaultController(SettingsProvider settingsProvider, Repository repository) {
+        public DefaultController(SettingsProvider settingsProvider, Repository repository, ILogger<DefaultController> logger, IConfiguration configuration) {
             _settingsProvider = settingsProvider;
             _repository = repository;
+            _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet("/api/get-video-details/{videoId}"), Produces("application/json")]
